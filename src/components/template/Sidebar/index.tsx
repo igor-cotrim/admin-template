721a8b8292c9a653b777c2c2647ef346b4da576d
Igor Cotrim <igorcotrim.dev@gmail.com>
2022-04-11 22:16:43 -0300

    create component Title and Logo

--- src/components/template/Sidebar/index.tsx
+++ src/components/template/Sidebar/index.tsx
@@ -1,9 +1,15 @@
 import ItemSidebar from './item'
 import { IconAjustments, IconHome, IconNotification } from '../../icons'
+import Logo from '../Logo'
 
 const Sidebar = () => {
   return (
     <aside>
+      <div
+        className={`flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}
+      >
+        <Logo />
+      </div>
       <ul>
         <ItemSidebar url="/" text="Início" icon={<IconHome />} />
         <ItemSidebar url="/ajusts" text="Ajustes" icon={<IconAjustments />} />
--- src/components/template/Title/index.tsx
+++ src/components/template/Title/index.tsx
@@ -6,8 +6,8 @@ type TitleProps = {
 const Title = ({ subtitle, title }: TitleProps) => {
   return (
     <div>
-      <h1>{title}</h1>
-      <h2>{subtitle}</h2>
+      <h1 className="font-black text-3xl text-gray-900">{title}</h1>
+      <h2 className="font-light text-sm text-gray-600">{subtitle}</h2>
     </div>
   )
 }
