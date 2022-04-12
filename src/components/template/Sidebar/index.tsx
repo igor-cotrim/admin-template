1f7388399148936a61662b66920807c3ad887fa7
Igor Cotrim <igorcotrim.dev@gmail.com>
2022-04-11 22:27:15 -0300

    add item to logout

--- src/components/template/Sidebar/index.tsx
+++ src/components/template/Sidebar/index.tsx
@@ -1,22 +1,35 @@
-import ItemSidebar from './item'
-import { IconAjustments, IconHome, IconNotification } from '../../icons'
 import Logo from '../Logo'
+import ItemSidebar from './item'
+import {
+  IconAjustments,
+  IconHome,
+  IconLogout,
+  IconNotification
+} from '../../icons'
 
 const Sidebar = () => {
   return (
-    <aside>
+    <aside className={`flex flex-col`}>
       <div
         className={`flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}
       >
         <Logo />
       </div>
-      <ul>
+      <ul className={`flex-grow`}>
         <ItemSidebar url="/" text="Início" icon={<IconHome />} />
         <ItemSidebar url="/ajusts" text="Ajustes" icon={<IconAjustments />} />
         <ItemSidebar
           url="/notifications"
           text="Notificações"
           icon={<IconNotification />}
+        />
+      </ul>
+      <ul>
+        <ItemSidebar
+          onClick={() => console.log('logout')}
+          className={`text-red-600 hover:bg-red-400 hover:text-white`}
+          text="Sair"
+          icon={<IconLogout />}
         />
       </ul>
     </aside>
--- src/components/template/Sidebar/item.tsx
+++ src/components/template/Sidebar/item.tsx
@@ -1,20 +1,39 @@
 import Link from 'next/link'
 
 type ItemSidebarProps = {
-  url: string
+  url?: string
   text: string
   icon: React.ReactNode
+  className?: string
+  onClick?: (event: any) => void
 }
 
-const ItemSidebar = ({ icon, text, url }: ItemSidebarProps) => {
+const ItemSidebar = ({
+  icon,
+  text,
+  url,
+  onClick,
+  className
+}: ItemSidebarProps) => {
   return (
-    <li className="hover:bg-gray-100">
-      <Link href={url}>
-        <a className="flex flex-col justify-center items-center h-20 w-20">
+    <li onClick={onClick} className={`hover:bg-gray-100 cursor-pointer`}>
+      {url ? (
+        <Link href={url}>
+          <a
+            className={`flex flex-col justify-center items-center text-gray-600 h-20 w-20 ${className}`}
+          >
+            {icon}
+            <span className={`text-xs font-light`}>{text}</span>
+          </a>
+        </Link>
+      ) : (
+        <a
+          className={`flex flex-col justify-center items-center text-gray-600 h-20 w-20 ${className}`}
+        >
           {icon}
-          <span className="text-xs font-light text-gray-600">{text}</span>
+          <span className={`text-xs font-light`}>{text}</span>
         </a>
-      </Link>
+      )}
     </li>
   )
 }
