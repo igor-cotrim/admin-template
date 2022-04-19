ca0a1abb58e7f51cb90a52951247d9ce2977c193
Igor Cotrim <igorcotrim.dev@gmail.com>
2022-04-18 22:53:35 -0300

    create button to change theme

--- src/components/template/Header/index.tsx
+++ src/components/template/Header/index.tsx
@@ -1,3 +1,5 @@
+import useAppData from '../../../data/hooks/useAppData'
+import ChangeThemeButton from '../ChangeThemeButton'
 import Title from '../Title'
 
 type HeaderProps = {
@@ -5,10 +7,17 @@ type HeaderProps = {
   subtitle: string
 }
 
-const Header = ({ subtitle, title }: HeaderProps) => (
-  <div>
-    <Title title={title} subtitle={subtitle} />
-  </div>
-)
+const Header = ({ subtitle, title }: HeaderProps) => {
+  const { onChange, theme } = useAppData()
+
+  return (
+    <div className={`flex`}>
+      <Title title={title} subtitle={subtitle} />
+      <div className={`flex flex-grow justify-end`}>
+        <ChangeThemeButton theme={theme} ChangeTheme={onChange} />
+      </div>
+    </div>
+  )
+}
 
 export default Header
